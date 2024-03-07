cafb5538862cdc47e2d4e11f32bd77e0de3d031d
Pisti0910 <150019895+Pisti0910@users.noreply.github.com>
2024-03-07 10:17:50 +0100

    Add files via upload

---
+++ alap.js
@@ -0,0 +1,39 @@
+fetch("https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json")
+.then(x => x.json())
+.then(y => megjelenit(y));
+var tomb=[]
+
+function megjelenit(y){
+    console.log(y)
+    tomb=y
+    var szoveg=""
+    var index=0;
+    for (const elem of y) {
+        szoveg+=`
+
+        
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+            <div class="kepek">
+                 <div class="felsoNev">${elem.name}</div>
+                    <img src="${elem.image}" alt="${elem.name}" title="${elem.name}" class="img-fluid kiskep">
+                <button id="${index}" onclick="kattintas(this)" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#myModal">
+                    ${elem.name}
+                </button>
+            </div>
+        </div>`
+        index++
+    }
+    document.getElementById("nevek").innerHTML=szoveg
+}
+function kattintas(gomb){
+    //alert(gomb.id)
+    document.getElementById("cim").innerHTML=tomb[gomb.id].name
+    document.getElementById("tartalom").innerHTML=`
+    <img src="${tomb[gomb.id].image}" alt="" style="margin-bottom">
+    <div><b>Születési idő: </b>${tomb[gomb.id].dateOfBirth}</div>
+    <div><b>Ház: </b>${tomb[gomb.id].house}</div>
+    <div><b>Patrónus: </b>${tomb[gomb.id].patronus}</div>
+    <div><b>Pálca anyaga: </b>${tomb[gomb.id].wand.wood}</div>
+    <div><b>Pálca közepe: </b>${tomb[gomb.id].wand.core}</div>
+    `
+}
